73fa675a869fafa1708d616a9efdcc340ecaedf0
Muhammad Umar <manaffayyaz4@gmail.com>
2024-05-01 23:03:03 +0500

    explore fix

--- src/screens/Explore/Styles.tsx
+++ src/screens/Explore/Styles.tsx
@@ -1,13 +1,19 @@
 import {StyleSheet} from 'react-native';
 import {COLORS} from '../../shared/theme';
 
-import {hp} from '../../utils/dimensionUtils/dimensions';
+import {hp, wp} from '../../utils/dimensionUtils/dimensions';
 
 const style = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLORS.primary,
+  },
+  backGroundImg: {height: hp('100'), width: wp('100')},
+  txtView: {
+    bottom: 40,
+    position: 'absolute',
     alignItems: 'center',
+    marginHorizontal: wp('5'),
   },
   exploreTxt: {color: COLORS.white, marginTop: hp('2')},
 });
