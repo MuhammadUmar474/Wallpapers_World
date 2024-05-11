4f591ef7887d8e11e6fe73e1d210f745b5f21f7f
MaryamShakir <123469567+MaryamShakir@users.noreply.github.com>
2024-05-11 22:39:10 +0500

    More App dark mode completed

--- src/screens/More/MoreApps/MoreApps.tsx
+++ src/screens/More/MoreApps/MoreApps.tsx
@@ -1,38 +1,44 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {FlatList, Image, Linking, TouchableOpacity, View} from 'react-native';
 
 import style from './Styles';
-import { hp, wp } from '../../../utils/dimensionUtils/dimensions';
-import { Text18, Text24 } from '../../../components/Text/Text';
-import { COLORS } from '../../../shared/theme';
-import { MoreAppsList } from '../../../assets/data/StaticData/StaticData';
+import {Text18, Text24} from '../../../components/Text/Text';
+import {MoreAppsList} from '../../../assets/data/StaticData/StaticData';
+import AppContext from '../../../context/AppContext';
 
+interface MoreAppsProps {
+  selectedColor: boolean;
+  listText: object;
+}
 
+const MoreApps: React.FC<MoreAppsProps> = () => {
+  const {selectedColor} = useContext(AppContext);
+  const onPress = (item: any) => {
+    Linking?.openURL(item?.url);
+  };
 
-const MoreApps = () => {
-
-  const onPress=(item:any)=>{
-      Linking?.openURL(item?.url)
-  }
-
-  const renderItem = ({item})=>{
-    return(
-        <TouchableOpacity onPress={()=>{onPress(item)}}>
-      <View style={style?.listItem}>
-        <Image source={item?.appIcon} style={style?.listImg}/>
-        <Text18 textStyle={style?.listText}>{item?.name}</Text18>
-      </View>
-    </TouchableOpacity>
-    )
-  }
-  return (
-    <View style={style.container}>
-        <View style={style?.InnerContainer}>
-
-            <Text24 textStyle={style?.Heading}>Our More Apps</Text24>
-            <FlatList  data={MoreAppsList} renderItem={renderItem}/>
+  const renderItem = ({item}: any) => {
+    return (
+      <TouchableOpacity
+        onPress={() => {
+          onPress(item);
+        }}>
+        <View style={style?.listItem}>
+          <Image source={item?.appIcon} style={style?.listImg} />
+          <Text18 textStyle={style?.listText(selectedColor)}>
+            {item?.name}
+          </Text18>
         </View>
+      </TouchableOpacity>
+    );
+  };
+  return (
+    <View style={style.container(selectedColor)}>
+      <View style={style?.InnerContainer}>
+        <Text24 textStyle={style?.Heading(selectedColor)}>Our More Apps</Text24>
+        <FlatList data={MoreAppsList} renderItem={renderItem} />
       </View>
+    </View>
   );
 };
 
