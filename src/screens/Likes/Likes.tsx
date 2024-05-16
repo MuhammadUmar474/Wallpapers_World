98718fcdb876abe6bcf84e04e2e0360ff20bced4
MaryamShakir <123469567+MaryamShakir@users.noreply.github.com>
2024-05-16 22:44:50 +0500

    like feature completed

--- src/screens/Likes/Likes.tsx
+++ src/screens/Likes/Likes.tsx
@@ -1,34 +1,105 @@
-import React, { useContext, useState } from 'react';
-import {FlatList, View} from 'react-native';
+import React, { useCallback, useContext, useState, useEffect } from 'react';
+import { FlatList, TouchableOpacity, View, ListRenderItem } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
+import FastImage from 'react-native-fast-image';
 
 import style from './Styles';
 import HorizontalView from '../../components/HorizontalView/HorizontalView';
-import {COLORS} from '../../shared/theme';
-import {hp, wp} from '../../utils/dimensionUtils/dimensions';
-import {Text24} from '../../components/Text/Text';
-import WallpaperComp from '../../components/WallpaperComp/WallpaperComp';
+import { COLORS } from '../../shared/theme';
+import { hp, wp } from '../../utils/dimensionUtils/dimensions';
+import { Text24 } from '../../components/Text/Text';
 import AppContext from '../../context/AppContext';
+import { getItemFromAsyncStorage, saveItemInAsyncStorage } from '../../utils/storage/asyncStorage';
+import { FontAwesome } from '../../shared/vectorIcons';
+import { navigate } from '../../navigation/rootNavigation';
 
-const Likes = () => {
-  const [selectedItem, setSelectedItem] = useState<string>('');
-  const {selectedColor} = useContext(AppContext)
+interface LikedItem {
+  id: string;
+  src: {
+    original: string;
+    medium: string;
+  };
+}
+
+const Likes: React.FC = () => {
+  const [likedItems, setLikedItems] = useState<LikedItem[]>([]);
+  const { selectedColor } = useContext(AppContext);
+
+  // FETCH STORE IMAGES FROM ASYNC STORAGE
+  const getLikedItemList = async () => {
+    const likedItemList = await getItemFromAsyncStorage('likeListItem');
+    if (likedItemList) {
+      const parsedLikedItems: LikedItem[] = JSON.parse(likedItemList);
+      setLikedItems(parsedLikedItems);
+    }
+  };
+
+  // DISLIKE QUOTE
+  const removeLikeItems = async (item: LikedItem) => {
+    const filterData = likedItems.filter((items) => item.id !== items.id);
+    await saveItemInAsyncStorage('likeListItem', JSON.stringify(filterData));
+    setLikedItems(filterData);
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      getLikedItemList();
+    }, [])
+  );
+
+  const onWallPaperPress = (item: LikedItem) =>
+    navigate('Preview', { uri: item.src.original });
+
+  const renderItem: ListRenderItem<LikedItem> = ({ item }) => (
+    <TouchableOpacity
+      style={style.container1}
+      onPress={() => onWallPaperPress(item)}
+    >
+      <FastImage
+        source={{
+          uri: item.src.medium,
+        }}
+        style={style.imgStyle}
+      />
+      <TouchableOpacity
+        onPress={() => removeLikeItems(item)}
+        style={{
+          ...style.likeStyle,
+          backgroundColor: false ? COLORS.red : COLORS.transparent,
+          borderWidth: false ? 0 : 1,
+        }}
+      >
+        <FontAwesome name={'heart'} size={20} color={COLORS.red} />
+      </TouchableOpacity>
+    </TouchableOpacity>
+  );
+
   return (
     <View style={style.container}>
-      <View style={{height: hp('100'), backgroundColor: selectedColor ? COLORS.background : COLORS?.white}}>
-        <View style={{marginTop: hp('6'), marginHorizontal: wp('4')}}>
+      <View
+        style={{
+          height: hp('100'),
+          backgroundColor: selectedColor ? COLORS.background : COLORS.white,
+        }}
+      >
+        <View style={style.container2}>
           <HorizontalView>
-            <Text24 textStyle={{color:selectedColor ?  COLORS.white : COLORS?.background}}>HD Wallpapers</Text24>
+            <Text24
+              textStyle={{
+                color: selectedColor ? COLORS.white : COLORS.background,
+              }}
+            >
+              HD Wallpapers
+            </Text24>
           </HorizontalView>
           <FlatList
-            style={{marginTop: hp('2')}}
-            data={[{}, {}, {}, {}]}
+            style={{ marginTop: hp('2') }}
+            data={likedItems}
             numColumns={2}
-            showsHorizontalScrollIndicator={false}
-            renderItem={({item}) => (
-              <WallpaperComp
-                selectedItem={selectedItem}
-              />
-            )}
+            showsVerticalScrollIndicator={false}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+            onEndReachedThreshold={0.4}
           />
         </View>
       </View>
